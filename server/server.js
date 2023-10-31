f0768a617e052b472b1372b9e4adb88d5590448c
Colten Yorgason <coltdane98@gmail.com>
2023-10-31 16:37:08 -0600

    updated commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const port = 5173;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get('/', () => {})
+
+app.listen(port, () =>
+  console.log(`Running on server http://localhost:${port}/`)
+);
+
+
