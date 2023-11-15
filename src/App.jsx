c4a893c6b758c317c11f9bdc36f3e05c190721e3
Colten Yorgason <coltdane98@gmail.com>
2023-11-15 15:01:15 -0700

    add error page

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,4 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import LandingPage from './routes/landingpage'
-
-function App() {
-const router = createBrowserRouter([{
-  path: "/", element: <LandingPage />
-}])
-  return (
-    <>
-      <RouterProvider router={router}/>
-    </>
-  )
-}
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from "./routes/root"
+import ErrorPage from './error-page.jsx'
+
+function Main() {
+const router = createBrowserRouter([{
+  path: "/", element: <Root />,
+  errorElement: <ErrorPage />
+}])
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  )
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+export default Main
