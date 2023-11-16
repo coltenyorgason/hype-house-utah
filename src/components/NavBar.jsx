6463819a02f4926adb2eccea8c3a3101d65f8b96
Colten Yorgason <coltdane98@gmail.com>
2023-11-16 15:56:41 -0700

    added instagram link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,12 +7,12 @@ export default function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">BounceHouse</Navbar.Brand>
+        <Navbar.Brand href="/">Hype House Utah</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/products">products</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="/products">The Main Castle</Nav.Link>
+            <Nav.Link href="https://www.instagram.com/hypehouseutah/">Follow Us On Instagram</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
